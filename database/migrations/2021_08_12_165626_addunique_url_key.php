fcd0251c2672b1fe8b7b928919d93c791d7a75aa
Marc Garcia Torrent <garciatorrent96@gmail.com>
2021-08-13 10:54:21 +0200

    Wrong class name
    AdduniqueUrlKey

--- database/migrations/2021_08_12_165626_addunique_url_key.php
+++ database/migrations/2021_08_12_165626_addunique_url_key.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class UpdateUrlKeyLength extends Migration
+class AdduniqueUrlKey extends Migration
 {
     /**
      * Run the migrations.
